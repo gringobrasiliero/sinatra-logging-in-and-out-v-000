f8b131b163be3edf8aca5b7124d7adc39ccac460
Nolan Byrnes <nolanehs@gmail.com>
2018-01-22 19:27:15 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180122121533) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "balance"
+    t.float "balance"
   end
 
 end
